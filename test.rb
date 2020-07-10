2940fff4888a790088b72d8c54778ae04691b54a
Charlie Barth <charliepbarth@gmail.com>
2020-07-10 18:55:18 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,13 +1,13 @@
 def matches(array)
     word = hello
+    new_array = []
     tests = word.chars.sort.join
     array.each do |name|
       x = name.chars.sort.join
       if tests == x
-       puts 
-      else 
-        false 
+       new_array << x
       end
     end
+    puts new_array
   end
   matches(["elloh", "lleoh", "eloh", "hi"])
