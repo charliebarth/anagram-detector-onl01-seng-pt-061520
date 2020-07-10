1933b83dc68fe901b8fcbde41a39139d7a78ba00
Charlie Barth <charliepbarth@gmail.com>
2020-07-10 18:56:15 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -6,14 +6,14 @@ class Anagram
   end
   
   def match(array)
+    new_array = []
     tests = @word.chars.sort.join
     array.each do |name|
       x = name.chars.sort.join
       if tests == x
-        true 
-      else 
-        false 
+        new_array << x
       end
     end
+    x
   end
 end
